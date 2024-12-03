d5676d3bda14613b3043688b4d47a6e4b61f5d86
Brandon Marchena <brandon.marchena@aplinet.co>
2024-12-02 23:40:25 -0500

    Ajustes reestructuracion proyecto

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,34 +1,26 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom"; // Importar Link para la navegación
-import "./Navbar.css";
-import logo from "./assets/logo.png"; // Importa la imagen desde src
+import "../styles/Navbar.css";
+import logo from "../assets/logo.png"; // Importa la imagen desde src
+import SearchBar from './SearchBar';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div className="navbar">
-      <Link to="/"> 
-        <div className="nav_logo">
-          <img src={logo} alt="CINEFAN Logo" className="logo_image" />
-        </div>
-      </Link>
+      <div className="nav_logo">
+        <img src={logo} alt="CINEFAN Logo" className="logo_image" />
+      </div>
       <div className={`nav_items ${isOpen && "open"}`}>
         {/* Cambiamos la etiqueta a Link para la navegación */}
         <Link to="/index-movie"> INICIO </Link>
-        <Link to={`/movie-list`}> PELICULAS </Link>
-        <div className="nav_search">
-          <input type="text" placeholder="Buscar..." className="search_input" />
-        </div>
+        <SearchBar />
         <div className="nav_user">
           {/* Usamos Link para redirigir a la página de login */}
           <Link to="/login">
             <i className="fas fa-user"></i> {/* Ícono de Font Awesome */}
           </Link>
-        </div>
-        <div className="nav_movie">
-          {/* Usamos Link para redirigir a la página de login */}
-          
         </div>
       </div>
 
